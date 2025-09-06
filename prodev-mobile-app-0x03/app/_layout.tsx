0dc39c74b0f2cdc258111eeafef1c372318afbae
ylb43u <mesterrachid2002@gmail.com>
2025-09-06 20:59:42 +0100

    initial commit

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,8 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack
-  screenOptions={{
+  return <Stack screenOptions={{
     headerShown: false
   }} />;
 }
